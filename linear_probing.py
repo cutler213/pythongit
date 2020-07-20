87a229adf2bbe9e05bbefd3df72002f2e9ebfad0
cutler213 <sanjeevanj.singh2@gmail.com>
2020-07-20 15:57:12 +0530

    comitting Data structures

--- linear_probing.py
+++ linear_probing.py
@@ -24,16 +24,35 @@ def delete(data):
             return 0
     arr[index]=-1
 
+def quadratic_probing(data):
+    global arr
+    size = len(arr)
+    key = data % size
+    index = key
+    i=1
+    if -1 in arr:
+        while arr[index]!=-1:
+            index=(index+pow(i,2))%size
+            i+=1
+        arr[index]=data
+
+
 
 print(arr)
-insert(10)
-insert(15)
-insert(2)
-insert(5)
-insert(4)
+quadratic_probing(10)
+quadratic_probing(15)
+quadratic_probing(2)
+quadratic_probing(5)
+quadratic_probing(4)
+quadratic_probing(43)
+#insert(10)
+#insert(15)
+#insert(2)
+#insert(5)
+#insert(4)
 #insert(20)
 print(arr)
-delete(20)
+#delete(20)
 print(arr)
 
 
